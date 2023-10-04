67a93f94a4a8fe302446d6cf795e59df3fb156aa
Steven <zhangc2@illinois.edu>
2023-10-03 23:08:21 -0500

    updated math formulas

--- app.py
+++ app.py
@@ -3,30 +3,42 @@ import numpy as np
 from scipy.optimize import minimize
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
     data = request.json
-    # Extract data from the request and convert to appropriate numeric types
-    Li = float(data['Li'])
-    Ki = float(data['Ki'])
-    Aix = float(data['Aix'])
-    Aiy = float(data['Aiy'])
-    x = float(data['x'])
-    y = float(data['y'])
+
+    # Extract data from the request
+    Li, Ki, Aix, Aiy, alpha_x, alpha_y, beta_i = float(data['Li']), float(data['Ki']), float(data['Aix']), float(data['Aiy']), float(data['alpha_x']), float(data['alpha_y']), float(data['beta_i'])
 
     # Define the PPF function
     def ppf(qx):
-        return Aiy * (Li - qx/Aix)**y * Ki**(1-y)
+        return Aiy * (Li - qx/Aix)**alpha_y * Ki**(1-alpha_y)
+
+    # Utility function
+    def utility(cy):
+        return cy**beta_i * cy**(1-beta_i)
+
+    # Optimization problem to find the PPF
+    def objective(q):
+        qx, qy = q
+        return -utility(qy)
+
+    constraints = (
+        {'type': 'eq', 'fun': lambda q: q[0] - Aix * q[1]**alpha_x * Ki**(1-alpha_x)},
+        {'type': 'ineq', 'fun': lambda q: Li - q[1]},
+        {'type': 'ineq', 'fun': lambda q: Ki - q[0]}
+    )
+
+    result = minimize(objective, [0.5, 0.5], constraints=constraints)
 
     # Return the calculated data
     return jsonify({
-        'qx': np.linspace(0, Aix*Li**x*Ki**(1-x), 100).tolist(),
-        'qy': [ppf(qxi) for qxi in np.linspace(0, Aix*Li**x*Ki**(1-x), 100)]
+        'qx': np.linspace(0, Aix*Li**alpha_x*Ki**(1-alpha_x), 100).tolist(),
+        'qy': [ppf(qxi) for qxi in np.linspace(0, Aix*Li**alpha_x*Ki**(1-alpha_x), 100)],
+        'utility': utility(result.x[1])
     })
 
 if __name__ == '__main__':
