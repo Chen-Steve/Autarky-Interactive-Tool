14d6860df8c548845add0b7a4672963ef1f2298a
Steven <zhangc2@illinois.edu>
2023-10-04 00:06:13 -0500

    added utility graph (need to check if its correct)

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ function generateGraph() {
     const Ki = document.getElementById('Ki').value;
     const Aix = document.getElementById('Aix').value;
     const Aiy = document.getElementById('Aiy').value;
-    const x = document.getElementById('x').value;
-    const y = document.getElementById('y').value;
+    const alpha_x = document.getElementById('x').value;
+    const alpha_y = document.getElementById('y').value;
+    const beta_i = document.getElementById('beta_i').value;  
 
     fetch('http://localhost:5000/calculate', {
         method: 'POST',
@@ -16,21 +17,34 @@ function generateGraph() {
             Ki: Ki,
             Aix: Aix,
             Aiy: Aiy,
-            x: x,
-            y: y
+            alpha_x: alpha_x,
+            alpha_y: alpha_y,
+            beta_i: beta_i
         })
     })
     .then(response => response.json())
     .then(data => {
-        const trace = {
+        const ppfTrace = {
             x: data.qx,
             y: data.qy,
             mode: 'lines',
             name: 'PPF'
         };
 
+        // Assuming utility is a single value, we'll plot it as a point on the graph
+        const utilityTrace = {
+            x: [data.utility],
+            y: [data.utility],
+            mode: 'markers',
+            name: 'Utility',
+            marker: {
+                size: 10,
+                color: 'red'
+            }
+        };
+
         const layout = {
-            title: 'Production Possibilities Frontier (PPF)',
+            title: 'Production Possibilities Frontier (PPF) with Utility',
             xaxis: {
                 title: 'Quantity of Good x'
             },
@@ -39,7 +53,7 @@ function generateGraph() {
             }
         };
 
-        Plotly.newPlot('plot', [trace], layout);
+        Plotly.newPlot('plot', [ppfTrace, utilityTrace], layout);
     })
     .catch(error => {
         console.error('Error fetching data:', error);
