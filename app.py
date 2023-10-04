7ebd4be7f9983c4b606ef28955a781b0315de1ff
Steven <zhangc2@illinois.edu>
2023-10-04 14:16:28 -0500

    heroku update

--- app.py
+++ app.py
@@ -60,4 +60,5 @@ def calculate():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
